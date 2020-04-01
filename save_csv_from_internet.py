99db8b3be09d09e84da2750f06291e8f57c47d79
taro350 <tarou44saika@gmail.com>
2020-03-31 22:50:33 -0700

    complete

--- save_csv_from_internet.py
+++ save_csv_from_internet.py
@@ -5,12 +5,12 @@ import tkinter as tk
 from tkinter import filedialog
 
     
-def save_file_csv(csv_from_internet):
+def convert_to_df(csv_from_internet):
     data = requests.get(csv_from_internet).text
     df = pd.read_csv(io.StringIO(data))
     return df
 
-def a(df):
+def save_csv(df):
     root = tk.Tk()
     canvas = tk.Canvas(root, width=300, height=300)
     canvas.pack()
